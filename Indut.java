3446b9e9df5dfe850496c1a21f34eefb06b6cec0
VivianoParaguayo <acosta@discente.ufg.br>
2024-07-03 21:42:09 -0300

    Add files via upload

---
+++ Indut.java
@@ -0,0 +1,15 @@
+@author (VivianoParaguayo) 
+@version (0.01)
+
+public class Indut extends Comp{
+    
+    private double indutancia;
+      
+    public Indut(double L){
+        this.indutancia = L;
+    }
+           
+    public void set_indutancia(double L){
+        this.indutancia = L;
+    }
+}
